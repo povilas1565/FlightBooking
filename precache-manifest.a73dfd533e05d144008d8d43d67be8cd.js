3478f2d3037a8032a591422d484c0a852d240a15
povilas1565 <pavel.ryzhkov.99@mail.ru>
2023-03-16 14:24:34 +0300

    Updates

---
+++ precache-manifest.a73dfd533e05d144008d8d43d67be8cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30be6d4ba92ccae08f46f9a23bd1eb19",
+    "url": "/FlightBooking/index.html"
+  },
+  {
+    "revision": "da90c1af52e1d2013255",
+    "url": "/FlightBooking/static/css/2.e31a430a.chunk.css"
+  },
+  {
+    "revision": "e990182f721beeae1a32",
+    "url": "/FlightBooking/static/css/main.285ea16c.chunk.css"
+  },
+  {
+    "revision": "da90c1af52e1d2013255",
+    "url": "/FlightBooking/static/js/2.cf21cc97.chunk.js"
+  },
+  {
+    "revision": "e990182f721beeae1a32",
+    "url": "/FlightBooking/static/js/main.2a652f7f.chunk.js"
+  },
+  {
+    "revision": "36ac13fc55694c407159",
+    "url": "/FlightBooking/static/js/runtime~main.af035dc3.js"
+  },
+  {
+    "revision": "9f0294234cf99a41a2ef4b7c50b11279",
+    "url": "/FlightBooking/static/media/multiflight.9f029423.png"
+  },
+  {
+    "revision": "a8b2e228713c48f9f76936176e857551",
+    "url": "/FlightBooking/static/media/nonstop.a8b2e228.png"
+  }
+]);
